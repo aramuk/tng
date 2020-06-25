574178352707f4a381ea6944988df86b9a55ce71
Aditesh Kumar <27091329+aramuk@users.noreply.github.com>
2020-06-25 11:52:30 -0700

    More abstraction

--- src/request/TNGRequest.py
+++ src/request/TNGRequest.py
@@ -17,18 +17,16 @@ TNG_SIMULATIONS = {"TNG100-1", "TNG100-1-Dark", "TNG100-2", "TNG100-2-Dark", "TN
 
 class TNGRequest():
 
-    def __init__(self, simulation: str, keyfile: str):
+    BASE_URL = f'http://www.tng-project.org/api/'
+
+    def __init__(self, keyfile: str):
         """Creates a TNGRequest that queries the passed 'simulation' name from Illustris-TNG using the API key in 'keyfile'"""
-        if simulation not in TNG_SIMULATIONS:
-            raise ValueError(f'{simulation} is not a valid TNG simulation')
-        self.simulation = simulation
-        self.base_url = f'http://www.tng-project.org/api/{simulation}/'
         self.headers = json.loads(open(keyfile).read()) 
     
 
     def get(self, endpoint, params=None, logfile=None, filename=None):
         """Makes a GET request to the API using the credentials provided"""
-        r = requests.get(self.base_url + endpoint, params=params, headers=self.headers)
+        r = requests.get(self.BASE_URL + endpoint, params=params, headers=self.headers)
         r.raise_for_status()
         if logfile:
             logfile.write(f'[{r.headers["Date"]}] GET {r.url} {r.status_code} {r.headers["Content-Length"]}' + '\n')
@@ -44,7 +42,7 @@ class TNGRequest():
         return r
 
 
-    def list_snapshot(self, snapshot=99, start=0, quantity=100, sort_params=dict(), logfile=None):
+    def list_snapshot(self, sim='TNG100-1', snapshot=99, start=0, quantity=100, sort_params=dict(), logfile=None):
         """Gets the requested snapshot data from TNG100-1"""
         endpoint = f'snapshots/{snapshot}/subhalos/'
         sort_params['limit'] = quantity if type(quantity) == int and 1 <= quantity <= 100 else 100
@@ -52,13 +50,13 @@ class TNGRequest():
         return self.get(endpoint, sort_params, logfile=logfile)
 
 
-    def get_subhalo_json(self, snapshot, subhalo_id, logfile=None):
+    def get_subhalo_json(self, sim, snapshot, subhalo_id, logfile=None):
         """Gets JSON data for requested subhalo"""
-        endpoint = f'snapshots/{snapshot}/subhalos/{subhalo_id}'
+        endpoint = f'{sim}/snapshots/{snapshot}/subhalos/{subhalo_id}'
         return self.get(endpoint, logfile=logfile)
 
 
-    def get_subhalo_image(self, snapshot, subhalo, params=dict(), logfile=None):
+    def get_subhalo_image(self, sim, snapshot, subhalo, params=dict(), logfile=None):
         """Gets the visualization for the galaxy specified by the snapshot and subhalo"""
         DOWNLOAD_PARAMS = {
             'partType': 'stars',
@@ -72,6 +70,6 @@ class TNGRequest():
             'plotStyle':'open'
         }
         DOWNLOAD_PARAMS.update(params)
-        endpoint = f'/snapshots/{snapshot}/subhalos/{subhalo}/vis.png'
+        endpoint = f'{sim}/snapshots/{snapshot}/subhalos/{subhalo}/vis.png'
         response = self.get(endpoint, DOWNLOAD_PARAMS, logfile)
         return Image.open(BytesIO(response.content))
